c52bd0fa94831af3d8d28565873e8d5699a2ad85
juan.jusue <juan.jusue@live.u-tad.com>
2018-02-19 09:59:23 +0100

    creado mainactivityevents para usar los listeners

--- app/src/main/java/com/example/juanjusue/examenjuanmajusue2t/MainActivity.java
+++ app/src/main/java/com/example/juanjusue/examenjuanmajusue2t/MainActivity.java
@@ -3,11 +3,40 @@ package com.example.juanjusue.examenjuanmajusue2t;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.juanjusue.examenjuanmajusue2t.FireBase.FireBaseAdminListener;
+import com.example.juanjusue.examenjuanmajusue2t.asynctasks.HttpJsonAsyncTaskListener;
+import com.example.juanjusue.examenjuanmajusue2t.sqllite.DataBaseHandler;
+import com.google.firebase.database.DataSnapshot;
+
 public class MainActivity extends AppCompatActivity {
+    public DataBaseHandler databaseHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}
+class MainActivityEvents implements HttpJsonAsyncTaskListener, FireBaseAdminListener {
+
+    @Override
+    public void firebaseAdmin_registerOk(Boolean blOk) {
+
+    }
+
+    @Override
+    public void firebaseAdmin_loginOk(Boolean blOk) {
+
+    }
+
+    @Override
+    public void firebaseAdmin_ramaDescargada(String rama, DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void JsonOk(String s) {
+
     }
 }
