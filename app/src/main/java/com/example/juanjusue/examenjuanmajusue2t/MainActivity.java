fe0e9b39f3a6e9b57c95706efa73eb3d4baf661a
juan.jusue <juan.jusue@live.u-tad.com>
2018-02-19 10:11:57 +0100

    añadimos al SQLLite los contactos descargados con asyncktask, desde el metodo de la interfaz del asynctasklistener, implementado en el events de mainactivity.

--- app/src/main/java/com/example/juanjusue/examenjuanmajusue2t/MainActivity.java
+++ app/src/main/java/com/example/juanjusue/examenjuanmajusue2t/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.juanjusue.examenjuanmajusue2t;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.juanjusue.examenjuanmajusue2t.DataHolder.DataHolder;
 import com.example.juanjusue.examenjuanmajusue2t.FireBase.FireBaseAdmin;
 import com.example.juanjusue.examenjuanmajusue2t.FireBase.FireBaseAdminListener;
 import com.example.juanjusue.examenjuanmajusue2t.asynctasks.HttpJsonAsyncTask;
 import com.example.juanjusue.examenjuanmajusue2t.asynctasks.HttpJsonAsyncTaskListener;
+import com.example.juanjusue.examenjuanmajusue2t.sqllite.Contact;
 import com.example.juanjusue.examenjuanmajusue2t.sqllite.DataBaseHandler;
+import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.database.DataSnapshot;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     public DataBaseHandler databaseHandler;
@@ -32,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 class MainActivityEvents implements HttpJsonAsyncTaskListener, FireBaseAdminListener {
+    MainActivity mainActivity;
 
     public MainActivityEvents(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
     }
 
     @Override
@@ -52,7 +61,23 @@ class MainActivityEvents implements HttpJsonAsyncTaskListener, FireBaseAdminList
     }
 
     @Override
-    public void JsonOk(String s) {
+    public void JsonOk(String x) {
+        Log.v("PRUEBA2",""+x);
+        try {
+
+            JSONObject object = new JSONObject(x); //Creamos un objeto JSON a partir de la cadena
+
+            JSONArray json_array = object.optJSONArray("Contactos");
+            for (int i = 0; i < json_array.length(); i++) {
+                this.mainActivity.databaseHandler.addContact(new Contact(Integer.parseInt(json_array.getJSONObject(i).getString("id")),json_array.getJSONObject(i).getString("nombre"),Double.parseDouble(json_array.getJSONObject(i).getString("lat")),Double.parseDouble(json_array.getJSONObject(i).getString("lon"))));
+                Log.v("PRUEBA1","ENTRA EN EL FOR"+json_array.getJSONObject(i).getString("nombre"));
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            FirebaseCrash.report(new Exception("Error JSON"));
+        }
 
     }
 }
