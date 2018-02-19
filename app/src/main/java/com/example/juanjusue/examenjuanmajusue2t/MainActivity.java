184ef28b7490dbd3131a1ca1238986ab41913064
juan.jusue <juan.jusue@live.u-tad.com>
2018-02-19 10:06:58 +0100

    añadido codigo necesario en MainActivity para la descarga de datos del mysql con php. Lo imprime bien.

--- app/src/main/java/com/example/juanjusue/examenjuanmajusue2t/MainActivity.java
+++ app/src/main/java/com/example/juanjusue/examenjuanmajusue2t/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.juanjusue.examenjuanmajusue2t;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.juanjusue.examenjuanmajusue2t.DataHolder.DataHolder;
+import com.example.juanjusue.examenjuanmajusue2t.FireBase.FireBaseAdmin;
 import com.example.juanjusue.examenjuanmajusue2t.FireBase.FireBaseAdminListener;
+import com.example.juanjusue.examenjuanmajusue2t.asynctasks.HttpJsonAsyncTask;
 import com.example.juanjusue.examenjuanmajusue2t.asynctasks.HttpJsonAsyncTaskListener;
 import com.example.juanjusue.examenjuanmajusue2t.sqllite.DataBaseHandler;
 import com.google.firebase.database.DataSnapshot;
@@ -16,9 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DataHolder.instance.fireBaseAdmin= new FireBaseAdmin();
+        MainActivityEvents events = new MainActivityEvents(this);
+        DataHolder.instance.fireBaseAdmin.setListener(events);
+
+        ////_________DESCARGAR JSON DESDE PHP-MYSQL___________\\\\\\\\\\\
+        HttpJsonAsyncTask httpJsonAsyncTask1=new HttpJsonAsyncTask(this);
+        httpJsonAsyncTask1.setListener(events);
+        String url1 = String.format("http://10.0.2.2/pruebasJSON/leeJugadores.php");
+        httpJsonAsyncTask1.execute(url1);
+        ////____________________________________________________\\\\\\\\\\
     }
 }
 class MainActivityEvents implements HttpJsonAsyncTaskListener, FireBaseAdminListener {
+
+    public MainActivityEvents(MainActivity mainActivity) {
+    }
 
     @Override
     public void firebaseAdmin_registerOk(Boolean blOk) {
